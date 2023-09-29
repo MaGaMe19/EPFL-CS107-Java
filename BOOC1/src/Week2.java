f626d76c48c747ad37919b843e72bd0eeb80b4df
MaGaMe19 <mattia.metzler@gmail.com>
2023-09-29 14:03:34 +0200

    beginning of code for week 2

---
+++ BOOC1/src/Week2.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Week2 {
+    public static void main(String[] args) {
+        // Chapitre 5 : structures de contrôle
+        // ================================================ branchements conditionnels
+        Scanner bob = new Scanner(System.in);
+        System.out.print("Donne moi un nombre : ");
+        int n = bob.nextInt();
+
+        if (n < 15) {
+            System.out.println("Ton nombre est plus petit que 15.");
+        } else {
+            System.out.println("Ton nombre est plus grand ou egal a 15.");
+        }
+
+        // Chapitre 6 : conditions
+        // ================================================ opérateurs de comparaison
+        /*
+        – <     qui signifie « inférieur à » ;
+        – >     qui signifie « supérieur à » ;
+        – ==    qui signifie « égal à » ;
+        – <=    qui signifie « inférieur ou égal à » ;
+        – >=    qui signifie « supérieur ou égal à » ;
+        – !=    qui signifie « différent de ».
+        */
+
+        // ================================================ opérateurs logiques
+
+    }
+}
