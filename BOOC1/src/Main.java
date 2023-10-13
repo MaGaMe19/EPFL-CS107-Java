2aba010227e661bec4d9626b6577146ff63b7786
MaGaMe19 <mattia.metzler@gmail.com>
2023-10-13 13:09:24 +0200

    changes

--- BOOC1/src/Main.java
+++ BOOC1/src/Main.java
@@ -1,9 +1,27 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        int num1 = -1;
-        int num2 = 2;
+        String s1 = "110";
+        String s2 = "11"                 ;
 
-        int average = num1 + num2;
-        System.out.println((double) average / 2);
+        int v = Integer.parseInt(s2);
+
+        for (int i = 0; i < s1.length(); i++){
+
+            String subs1 = s1.substring(0, i + 1);
+            String subs2 = s1.substring(i + 1, s1.length());
+
+            if (i == s1.length() - 1){
+                subs2 = "0";
+            }
+
+            int v1 = Integer.parseInt(subs1);
+            int v2 = Integer.parseInt(subs2);
+
+            if (v1 + v2 == v){
+                System.out.println(subs1 + " + " + subs2 + " = " + s2 );
+            }
+        }
     }
 }
