ecd699cafdb9afd15d8ae7908a14b8750af3b558
Mattia <mattia.metzler@gmail.com>
2023-10-18 13:23:53 +0200

    week 6

--- BOOC1/src/Week6.java
+++ BOOC1/src/Week6.java
@@ -1,5 +1,15 @@
 public class Week6 {
-    public static void main(String[] args) {
+    // Chapitre 22 : Méthodes (Introduction)
 
+    //            type  nom    paramètres
+    public static int crazyOp(int a, int b) { // entête
+        int c = a + b % 10;             // |
+        int d = c * a - b;              // | corps de la méthode
+        int e = d - a + c;              // |
+
+        return c * d + e;               // | valeur de retour
+    } // définition
+    public static void main(String[] args) {
+        System.out.println(crazyOp(12, 7)); // appel
     }
 }
