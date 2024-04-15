be1d4aa355c1e94f6a250a2b3fc28cde3923e5e8
Mattia <mattia.metzler@gmail.com>
2024-04-15 17:39:17 +0200

    goo goo ga ga

---
+++ BOOC2/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        int i = 3;
+        switch (i) {
+            case 1 -> {
+                System.out.println(i);
+            }
+            case 2 -> {
+                System.out.println(i + ".");
+            }
+        }
+
+    }
+}
